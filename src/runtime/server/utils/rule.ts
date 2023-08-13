17985d844e0b1eaf1d09dafc8b4814f9ed8caff5
OhB00 <43827372+OhB00@users.noreply.github.com>
2023-08-13 14:15:10 +0100

    Fix typings in rules

--- src/runtime/server/utils/rule.ts
+++ src/runtime/server/utils/rule.ts
@@ -1,14 +1,18 @@
 import { createRouter } from "radix3"
-import { rllogger } from "./rl"
 import type { KeyData } from "./key"
 import type { RateLimitRule } from "../../../module"
 import type { H3Event } from "h3"
 
 export type CustomRuleFunction = (
   event: H3Event,
-  data: KeyData,
+  data: RuleOverrideData,
   rule: RateLimitRule,
 ) => Promise<RateLimitRule>
+
+export interface RuleOverrideData {
+  key: string
+  metadata: Record<string, string>
+}
 
 export const customRuleRouter = createRouter<{ handler: CustomRuleFunction }>()
 
@@ -23,11 +27,14 @@ export async function getRule(
 ) {
   const customRule = customRuleRouter.lookup(event.path)
 
-  if (customRule === null) {
+  if (customRule === null || data.success === false) {
     return rule
   }
 
   delete customRule.params
 
-  return await customRule.handler(event, data, rule)
+  return await customRule.handler(event, {
+    key: data.key,
+    metadata: data.metadata
+  }, rule)
 }
